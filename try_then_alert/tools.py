cfcd9a88a8c084c639c17da34f8c07cc8340564d
Spencer Carrucciu <scarrucciu@remedypartners.com>
2015-09-22 21:55:02 -0400

    add optional subject argument to try_then_alert decorator

--- try_then_alert/tools.py
+++ try_then_alert/tools.py
@@ -1,29 +1,30 @@
 import smtplib
-from functools import wraps
+import functools
 from alert import get_gmail_credentials, send_gmail
 
 
-def try_then_alert(function):
-    @wraps(function)
+def try_then_alert(function=None, subject='An Error Occured'):
+    if function is None:
+        return functools.partial(try_then_alert, subject=subject)
+    @functools.wraps(function)
     def wrap(*args, **kwargs):
         try:
             return function(*args, **kwargs)
         except Exception as e:
             html = e.message
-            subject = 'An Error Occured'
             credentials = get_gmail_credentials()
             try:
-                send_gmail(me=credentials['email'], 
+                send_gmail(me=credentials['email'],
                            you=credentials['email'],
-                           password=credentials['password'], 
-                           html=html, 
+                           password=credentials['password'],
+                           html=html,
                            subject=subject)
 
             except Exception as email_e:
                 print '--------------------'
                 print 'Error Alert not sent'
                 print '--------------------'
-                print 'Email send error msg:'                
+                print 'Email send error msg:'
                 print email_e.message
                 print email_e.strerror
                 print '--------------------'
